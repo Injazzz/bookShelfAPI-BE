bab7285d71232f35897113d86f2db008bfdd3b82
injazzz <kamal.akbarzy@gmail.com>
2024-05-17 18:13:54 +0700

    restructuring lines

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,39 @@
-const { 
-    add_BooksHandler, 
-    getAll_BooksHandler,
-    getBookById_BooksHandler,
-    editBookById_BooksHandler,
-    deleteBookById_BooksHandler
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+const {
+  add_BooksHandler,
+  getAll_BooksHandler,
+  getBookById_BooksHandler,
+  editBookById_BooksHandler,
+  deleteBookById_BooksHandler,
 } = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/books',
-        handler: add_BooksHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books',
-        handler: getAll_BooksHandler,
-    },
-    {
-        method: 'GET',
-        path: '/books/{id}',
-        handler: getBookById_BooksHandler,
-    },
-    {
-        method: 'PUT',
-        path: '/books/{id}',
-        handler: editBookById_BooksHandler,
-    },
-    {
-        method: 'DELETE',
-        path: '/books/{id}',
-        handler: deleteBookById_BooksHandler,
-    }
+  {
+    method: 'POST',
+    path: '/books',
+    handler: add_BooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAll_BooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookById_BooksHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookById_BooksHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBookById_BooksHandler,
+  },
 ];
 
 module.exports = routes;
